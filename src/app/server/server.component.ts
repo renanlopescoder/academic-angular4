daad5db46297edd7797ef1ea87c0ff13ba7f68b6
Renan Lopes <renanlopescoder@gmail.com>
2017-05-30 13:01:45 -0300

    Feature: Data Binding
    feature: Built new components for practicing the concepts
    feature: Using data binding between template and TypeScript logic using components

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  // tslint:disable-next-line:no-inferrable-types
+  serverId: number = 10;
+  // tslint:disable-next-line:no-inferrable-types
+  serverStatus: string = 'Offline';
+  // tslint:disable-next-line:no-inferrable-types
+  allowNewServer: boolean = false;
 
+  constructor() {
+    setTimeout( () => this.allowNewServer = true
+    , 2000);
+  }
+
+  getServerStatus() { return this.serverStatus; };
 }
