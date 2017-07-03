6266d12429f60a573f0a234dabadfdce290bd326
Renan Lopes <renanlopescoder@gmail.com>
2017-07-03 17:29:38 -0300

    refactor: remoção dos components de teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { ProjetoModule } from './projeto/projeto.module';
 import { routing } from './app.routes';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
